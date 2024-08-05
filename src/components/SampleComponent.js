141f9aa659ffbf3dfd7d4adb32be8f0090372150
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-05 20:37:46 +0530

    feat: Add SampleComponent

---
+++ src/components/SampleComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SampleComponent() {
+  return (
+    <div className='text-red-700'>
+      Sample Component
+    </div>
+  )
+}
+
+export default SampleComponent
