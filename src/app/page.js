b05362029f87101592f015545ecb0ca553e8f823
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-05 20:37:11 +0530

    feat: Update landing page with recommended, currently playing, and upcoming sections

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,27 @@
-
+import Recommended from "../components/Landing/Recommended/Recomendation"
+import WhyChooseSection from "../components/Landing/WhyChoose"
+import Slider from "../components/Landing/Slider"
 export default function Home() {
   return (
-    <main className="flex h-screen w-full items-center flex-col  justify-center">
-  
-  <h1 className="text-7xl font-bold">B Wood</h1>
+    <main className="sm:px-10">
+  <Slider/>
+<WhyChooseSection />
+  <div className="p-6">
+    <h1 className="text-3xl text-[#a8ff35]  font-bold mb-6 m-2">Recommended</h1>
+  <Recommended/>
 
-    
-    </main>
+  </div>
+  <div className="p-6">
+    <h1 className="text-3xl text-[#a8ff35]  font-bold mb-6 m-2">Currently Playing</h1>
+  <Recommended/>
+
+  </div>
+  <div className="p-6">
+    <h1 className="text-3xl text-[#a8ff35]  font-bold mb-6 m-2">Upcoming</h1>
+  <Recommended/>
+
+  </div>
+  
+ </main>
   );
 }
