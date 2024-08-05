0087e7742b24b1bb6a3b9b778dc3ff123b0e02df
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-05 20:37:20 +0530

    feat: Add lazy loading for images in Movies page component

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import MovieSection from "../../components/MovieSection/movieSection"
+import CrewTable from "../../components/MovieSection/crew"
+import CastTable from '../../components/MovieSection/cast'
 function Movies() {
   return (
     <div>
-Movies
-    </div>
+<MovieSection/>
+<div className='lg:flex justify-evenly'>
+  
+  <CastTable/>
+  <CrewTable/>
+  </div> </div>
   )
 }
 
