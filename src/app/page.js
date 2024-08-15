e38b2bd38f2aecd58733fa4f85c5a0e140430aaa
Sambit Mondal <sambitmondal2005@gmail.com>
2024-08-16 00:40:00 +0530

    Update: Landing Page Responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -3,25 +3,25 @@ import WhyChooseSection from "../components/Landing/WhyChoose"
 import Slider from "../components/Landing/Slider"
 export default function Home() {
   return (
-    <main className="px-10">
-  <Slider/>
-<WhyChooseSection />
-  <div className="p-6">
-    <h1 className="text-3xl text-neon  font-bold mb-6 m-2">Recommended</h1>
-  <Recommended/>
+    <main className="w-full lg:px-10">
 
-  </div>
-  <div className="p-6">
-    <h1 className="text-3xl text-neon  font-bold mb-6 m-2">To be Watched</h1>
-  <Recommended/>
+      <Slider />
+      <WhyChooseSection />
 
-  </div>
-  <div className="p-6">
-    <h1 className="text-3xl text-neon  font-bold mb-6 m-2">Upcoming</h1>
-  <Recommended/>
+      <div className="p-6">
+        <h1 className="text-3xl text-neon font-bold mb-6 m-2">Recommended</h1>
+        <Recommended />
+      </div>
 
-  </div>
-  
- </main>
+      <div className="p-6">
+        <h1 className="text-3xl text-neon font-bold mb-6 m-2">To be Watched</h1>
+        <Recommended />
+      </div>
+
+      <div className="p-6">
+        <h1 className="text-3xl text-neon font-bold mb-6 m-2">Upcoming</h1>
+        <Recommended />
+      </div>
+    </main>
   );
 }
--- src/components/Landing/WhyChoose.js
+++ src/components/Landing/WhyChoose.js
@@ -5,34 +5,33 @@ const reasons = [
   {
     title: 'Exclusive Content',
     description: 'Get access to the hottest new movies as soon as they hit the big screen.',
-    icon: <FaNewspaper size={40} />,
+    icon: <FaNewspaper className='size-16' />,
   },
   {
     title: 'Premium Experience',
     description: 'Enjoy state-of-the-art theaters with cutting-edge sound and visual technology.',
-    icon: <FaTachometerAlt size={40} />,
+    icon: <FaTachometerAlt className='size-16' />,
   },
   {
     title: 'Easy Booking',
     description: 'Book your tickets in seconds with our user-friendly web app and website.',
-    icon: <FaCalendarAlt size={40} />,
+    icon: <FaCalendarAlt className='size-16' />,
   },
   {
     title: 'Exclusive Offers',
     description: 'Get access to members-only discounts, special screenings, and more.',
-    icon: <FaUserFriends size={40} />,
+    icon: <FaUserFriends className='size-16' />,
   },
 ];
 
 const WhyChooseSection = () => {
   return (
-    <section className="bg-gradient-to-r from-gradient-start to-gradient-end p-8 py-16">
-      {/* <h2 className="text-4xl font-bold text-neon mb-20 text-center">Why  Us?</h2> */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2  xl:grid-cols-4 gap-6">
+    <section className="w-full flex items-center justify-center bg-gradient-to-r from-gradient-start to-gradient-end p-8 py-16">
+      <div className="flex justify-center flex-wrap lg:flex-nowrap gap-6">
         {reasons.map((reason, index) => (
           <div
             key={index}
-            className="relative border border-white overflow-hidden rounded-lg bg-black text-white shadow-lg transform transition-transform duration-150 hover:scale-105"
+            className="relative flex items-center flex-col justify-around py-6 border border-white overflow-hidden rounded-lg bg-black text-white shadow-lg transform transition-transform duration-150 hover:scale-105"
           >
             <div className="flex items-center justify-center h-24 w-full bg-gradient-to-br from-gradient-start to-gradient-end transition-all duration-150">
               <div className="text-center">{reason.icon}</div>
@@ -41,7 +40,7 @@ const WhyChooseSection = () => {
               <h3 className="text-2xl text-center text-neon font-semibold mb-3">{reason.title}</h3>
               <p className="text-sm text-center">{reason.description}</p>
             </div>
-            <div className="absolute inset-0 gradient-overlay"></div>
+            <div className="absolute -z-10 inset-0 gradient-overlay"></div>
           </div>
         ))}
       </div>
