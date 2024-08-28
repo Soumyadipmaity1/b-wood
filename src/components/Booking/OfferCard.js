c048db14e054062e5de414284a37ab3592304d7b
Sambit Mondal <sambitmondal2005@gmail.com>
2024-08-28 14:09:34 +0530

    UI and resources Update

--- src/components/Booking/OfferCard.js
+++ src/components/Booking/OfferCard.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import offer from '../../../public/assets/offer.jpg'
 
 const OfferCard = () => {
     return (
         <div className='px-10 rounded-md w-1/3'>
             <div className="bg-neon text-black text-center w-full h-full">
-                <img src={offer} alt='Offer Card' className='w-full h-full' />
+                <img src='/offer.jpg' alt='Offer Card' className='w-full h-full' />
             </div>
         </div>
     );
