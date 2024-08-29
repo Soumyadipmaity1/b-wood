572a2e7de453af531b170897a6b29157bb0decf3
Sambit Mondal <sambitmondal2005@gmail.com>
2024-08-29 23:43:44 +0530

    Book tickets(Seat selection) page UI updated

--- src/app/[id]/booking/[sid]/page.js
+++ src/app/[id]/booking/[sid]/page.js
@@ -2,17 +2,30 @@
 import React, { useEffect, useState } from "react";
 import SeatSelection from "../../../../components/Ticket/SeatSelection";
 import { getShowTimeById } from "../../../../actions/theater";
+import { order } from "../../../../actions/razorpay.js";
+import RenderRazorpay from "../../../../components/Ticket/RenderRazorpay.js";
 
 function Ticket({ params }) {
   const { sid } = params;
   const [showtime, setShowtime] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [selectedSeats, setSelectedSeats] = useState([]);
+  const [orderDetails, setOrderDetails] = useState({
+    orderId: null,
+    currency: null,
+    amount: null,
+  });
+  const [displayRazorpay, setDisplayRazorpay] = useState(false);
+  const [seatPrice, setSeatPrice] = useState(200); // Default seat price
 
   useEffect(() => {
     const fetchShowtime = async () => {
       try {
         const res = await getShowTimeById(sid);
         setShowtime(res);
+        if (res.price) {
+          setSeatPrice(res.price);
+        }
       } catch (error) {
         console.error("Failed to fetch showtime:", error);
       } finally {
@@ -23,25 +36,101 @@ function Ticket({ params }) {
     fetchShowtime();
   }, [sid]);
 
+  const handleSeatClick = (seat) => {
+    // Handle seat selection logic here
+    if (showtime && showtime.reserved_seats.includes(seat)) return;
+
+    if (selectedSeats.includes(seat)) {
+      setSelectedSeats(selectedSeats.filter((s) => s !== seat));
+    } else {
+      setSelectedSeats([...selectedSeats, seat]);
+    }
+  };
+
+  const handleSubmit = async (amount) => {
+    try {
+      const res = await order(amount);
+      if (res && res.id) {
+        setOrderDetails({
+          orderId: res.id,
+          amount: res.amount,
+        });
+      }
+      setDisplayRazorpay(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const totalAmount = selectedSeats.length * seatPrice;
+
   if (loading) {
     return <div>Loading...</div>; // Handle loading state here
   }
 
   return (
-    <div className="mt-28 lg:px-20 sm:px-10">
-      <div className="sm:px-10 px-4 lg:pt-8 pt-4 pb-5 bg-[#131313]">
-        <h1 className="sm:text-4xl text-3xl text-white font-bold pb-4">
-          {showtime.movieId ? showtime.movieId.title : "Loading"}
-        </h1>
-        <p className="text-[#a8ff35]">
+    <div className="mt-20 lg:px-20 sm:px-10">
+      <div className="hidden lg:flex items-center justify-between sm:px-10 px-4 lg:pt-8 pt-4 pb-5">
+        <SeatSelection showtime={showtime} handleSeatClick={handleSeatClick} />
+        <div className="flex items-center justify-center flex-col w-full lg:w-3/4 ml-20">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="sm:text-4xl text-3xl text-white font-bold pb-4 text-center">
+              {showtime.movieId ? showtime.movieId.title : "Loading"}
+            </h1>
+            <p className="text-center">
+              {showtime.movieId ? showtime.movieId.description : "Loading"}
+            </p>
+          </div>
+          <p className="text-[#a8ff35] flex items-center justify-between w-full px-10 mt-6 mb-10 font-bold tracking-wide bg-[#131313] p-2 rounded-lg">
+            {showtime.theaterId ? showtime.theaterId.name : "Loading"}
+            <span className="ml-4 text-yellow-400">
+              {showtime.movieId ? showtime.movieId.duration : "Loading"}
+            </span>
+          </p>
+          <div className="flex justify-center mt-6">
+            <button
+              className="bg-[#00ff00] hover:bg-lime-600 cursor-pointer text-black font-bold py-2 xl:h-14 lg:h-16 h-14 xl:px-20 lg:px-10 px-20 rounded-lg"
+              disabled={totalAmount === 0}
+              onClick={() => handleSubmit(totalAmount)}
+            >
+              {totalAmount > 0 ? `Pay ${totalAmount} INR` : "Book Ticket"}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="lg:hidden sm:px-10 px-4 lg:pt-8 pt-4 pb-5 bg-[#131313]">
+        <div className="w-full flex flex-col items-center">
+          <h1 className="sm:text-4xl text-3xl text-white font-bold pb-4 text-center">
+            {showtime.movieId ? showtime.movieId.title : "Loading"}
+          </h1>
+          <p className="text-center">
+            {showtime.movieId ? showtime.movieId.description : "Loading"}
+          </p>
+        </div>
+        <p className="text-[#a8ff35] mt-6 w-full flex items-center justify-between">
           {showtime.theaterId ? showtime.theaterId.name : "Loading"}
           <span className="ml-4 text-yellow-400">
             {showtime.movieId ? showtime.movieId.duration : "Loading"}
           </span>
         </p>
       </div>
-      <hr className="pb-10 border-gray-400" />
-      <SeatSelection showtime={showtime}/>
+
+      <div className="lg:hidden">
+        <hr className="pb-10 border-gray-400 lg:hidden w-full mx-auto" />
+        <SeatSelection showtime={showtime} handleSeatClick={handleSeatClick} />
+      </div>
+
+      {displayRazorpay && (
+        <RenderRazorpay
+          amount={orderDetails.amount}
+          orderId={orderDetails.orderId}
+          keyId={"rzp_test_hAi2jfp9hhWAbU"}
+          keySecret={"q59gkQSAD7qKyu8Lis1YecuM"}
+          showtime={showtime}
+          selectedSeats={selectedSeats}
+        />
+      )}
     </div>
   );
 }
--- src/components/Ticket/SeatSelection.js
+++ src/components/Ticket/SeatSelection.js
@@ -1,27 +1,21 @@
 "use client";
 
-import { useState } from "react";
-import Link from "next/link";
-import { order } from "../../actions/razorpay.js";
-import RenderRazorpay from "./RenderRazorpay.js";
+import { useState, useEffect } from "react";
 
 const rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
 const seatsPerRow = 10;
 
-const SeatSelection = ({ showtime }) => {
+const SeatSelection = ({ showtime, handleSeatClick }) => {
   const [selectedSeats, setSelectedSeats] = useState([]);
   const { reserved_seats } = showtime; // Reserved seats from the showtime object
   const [bookedSeats] = useState(reserved_seats || []); // Initialize booked seats with reserved seats
-  const [orderDetails, setOrderDetails] = useState({
-    orderId: null,
-    currency: null,
-    amount: null,
-  });
-  const [displayRazorpay, setDisplayRazorpay] = useState(false);
-  let seatPrice = showtime.price || 200; // Set seat price based on showtime or default to 200
+  const seatPrice = showtime.price || 200; // Set seat price based on showtime or default to 200
 
-  const handleSeatClick = (seat) => {
-    // If the seat is booked, do nothing
+  useEffect(() => {
+    setSelectedSeats([]); // Clear selected seats when showtime changes
+  }, [showtime]);
+
+  const handleSeatSelection = (seat) => {
     if (bookedSeats.includes(seat)) return;
 
     // Toggle seat selection
@@ -30,33 +24,21 @@ const SeatSelection = ({ showtime }) => {
     } else {
       setSelectedSeats([...selectedSeats, seat]);
     }
-  };
 
-  const handleSubmit = async (amount) => {
-    try {
-      const res = await order(amount);
-      if (res && res.id) {
-        setOrderDetails({
-          orderId: res.id,
-          amount: res.amount,
-        });
-      }
-      setDisplayRazorpay(true);
-    } catch (error) {
-      console.log(error);
-    }
+    // Notify parent component about the seat selection
+    handleSeatClick(seat);
   };
 
   const totalAmount = selectedSeats.length * seatPrice;
 
   return (
     <>
-      <div className="text-white md:mt-10 mt-5 justify-center lg:gap-40 lg:flex-row lg:flex flex flex-col">
-        <div className="flex-col flex">
+      <div className="text-white md:mt-10 mt-5 justify-center lg:gap-40 lg:flex-row lg:flex flex w-full overflow-x-hidden">
+        <div className="flex-col flex bg-[#131313]   py-3 lg:p-5">
           <div className="w-64 h-24 mb-14 border-lime-500 border text-black font-bold bg-gray-400 items-center flex justify-center mx-auto">
             Screen
           </div>
-          <div className="mx-auto grid grid-cols-10 sm:gap-4 text-sm sm:text-base gap-2 md:justify-center">
+          <div className="mx-auto grid grid-cols-10 sm:gap-4 text-[0.7rem] sm:text-base gap-[0.3rem] lg:gap-3 md:justify-center w-full px-2 lg:px-0">
             {rows.map((row) =>
               Array.from({ length: seatsPerRow }).map((_, index) => {
                 const seat = `${row}${index + 1}`;
@@ -65,14 +47,14 @@ const SeatSelection = ({ showtime }) => {
                 const seatClass = isBooked
                   ? "bg-gray-500 cursor-not-allowed" // Change class to indicate it's not clickable
                   : isSelected
-                  ? "bg-[#00ff00] text-black"
-                  : "bg-transparent border-[#00ff00] border";
+                    ? "bg-[#00ff00] text-black"
+                    : "bg-transparent border-[#00ff00] border";
 
                 return (
                   <div
                     key={seat}
                     className={`${seatClass} sm:w-10 sm:h-10 w-8 h-8 rounded-md flex items-center justify-center`}
-                    onClick={() => handleSeatClick(seat)}
+                    onClick={() => handleSeatSelection(seat)}
                     style={{ cursor: isBooked ? "not-allowed" : "pointer" }} // Style to prevent pointer on booked seats
                   >
                     {seat}
@@ -82,7 +64,7 @@ const SeatSelection = ({ showtime }) => {
             )}
           </div>
 
-          <div className="flex justify-center my-4 space-x-4">
+          <div className="flex justify-center my-4 mt-6 space-x-4">
             <div className="flex items-center">
               <div className="bg-transparent border-[#00ff00] border w-4 h-4 rounded-sm mr-2"></div>
               <span>Available</span>
@@ -96,27 +78,17 @@ const SeatSelection = ({ showtime }) => {
               <span>Selected</span>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center lg:mt-0 mt-5 items-center">
-          <button
-            className="bg-[#00ff00] hover:bg-lime-600 cursor-pointer text-black font-bold py-2 xl:h-14 lg:h-16 h-14 xl:px-20 lg:px-10 px-20 rounded-lg"
-            disabled={totalAmount === 0}
-            onClick={() => handleSubmit(totalAmount)}
-          >
-            {totalAmount > 0 ? `Pay ${totalAmount} INR` : "Book Ticket"}
-          </button>
+          <div className="flex lg:hidden justify-center lg:mt-0 mt-5 items-center">
+            <button
+              className="bg-[#00ff00] hover:bg-lime-600 cursor-pointer text-black font-bold py-2 xl:h-14 lg:h-16 h-14 xl:px-20 lg:px-10 px-20 rounded-lg"
+              disabled={totalAmount === 0}
+              onClick={() => handleSubmit(totalAmount)}
+            >
+              {totalAmount > 0 ? `Pay ${totalAmount} INR` : "Book Ticket"}
+            </button>
+          </div>
         </div>
       </div>
-      {displayRazorpay && (
-        <RenderRazorpay
-          amount={orderDetails.amount}
-          orderId={orderDetails.orderId}
-          keyId={"rzp_test_hAi2jfp9hhWAbU"}
-          keySecret={"q59gkQSAD7qKyu8Lis1YecuM"}
-          showtime={showtime}
-          selectedSeats={selectedSeats}
-        ></RenderRazorpay>
-      )}
     </>
   );
 };
