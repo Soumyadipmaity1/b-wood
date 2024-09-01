0fa21fbdec5496d3f89e74505e9f6b56862c8082
Shashank Raj <rajshashank2104@gmail.com>
2024-09-01 15:18:30 +0530

    added user account details

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,7 +1,9 @@
 "use server";
 import User from "../db/models/user.js";
 import connectDB from "../db/database.js";
-
+import Reservation from "../db/models/reservation.js";
+import Showtime from "../db/models/showtime.js";
+import Movie from "../db/models/movie.js";
 export const getUser = async () => {
     await connectDB();
     try {
@@ -73,7 +75,21 @@ export const deleteUser = async (id) => {
 export const getUserbyEmail=async(email)=>{
     await connectDB();
     try {
-        const user = await User.findOne({email}).lean();
+        const user = await User.findOne({ email }).select("name email phone reservation -_id")
+        .populate({
+          path: "reservation",
+          select:"showtimeId amount -_id",
+          populate: {
+            path: "showtimeId", // This is the field inside reservation that we want to populate
+            model: "Showtime",
+            select:"movieId startAt reserved_seats -_id",
+            populate: {
+              path: "movieId", // This is the field inside showtime that we want to populate
+              model: "Movie",
+              select:"title -_id"
+            },
+          },
+        }).lean();        
         if (!user) {
             throw new Error("User not found");
         }
@@ -81,5 +97,33 @@ export const getUserbyEmail=async(email)=>{
     } catch (error) {
         console.error(error);
         throw new Error("Failed to fetch user by email");
+    }
+}
+
+
+export const updateUserByEmail = async(email,data) => {
+    await connectDB();
+    try {
+        const user = await User.findOneAndUpdate({ email }, { $set: { phone: data } }, { new: true}).lean();
+        if (!user) {
+            throw new Error("User not found");
+        }
+        return user;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deleteUserByEmail=async(email)=>{
+    await connectDB();
+    try {
+        const user = await User.findOneAndDelete({ email });
+        if (!user) {
+            throw new Error("User not found");
+        }
+        return { message: "User deleted successfully" };
+    } catch (error) {
+        console.error(error);
+        throw new Error("Failed to delete user by email");
     }
 }
